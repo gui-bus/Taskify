e514e2974253b2344f4c990794d79de08f842f81
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-09-25 17:41:48 -0300

    feat: add content to home & dashboard

---
+++ src/pages/dashboard/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Head>
+        <title>Taskify | Dashboard</title>
+      </Head>
+      <h1 className="text-2xl text-white">Dashboard</h1>
+    </div>
+  );
+}
