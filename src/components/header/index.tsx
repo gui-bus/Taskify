3697a7a29a123438bd1b7a9283aba7f16cf6d4ef
Guilherme Bustamante <guilherme_bustamante@live.com>
2023-09-27 19:56:59 -0300

    style: update styles

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,8 +3,9 @@ import Link from "next/link";
 import { useSession, signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import logo from "../../../public/assets/logo.png";
-import { FiLogIn, FiLogOut, FiMenu } from "react-icons/fi";
 import { CgSpinner } from "react-icons/cg";
+import { MdPublic } from "react-icons/md";
+import { TbListCheck, TbLogin2, TbLogout2, TbMenu2 } from "react-icons/tb";
 
 export function Header() {
   const { data: session, status } = useSession();
@@ -18,23 +19,37 @@ export function Header() {
     <header className="w-full max-w-[48.75] h-20 md:h-20 flex items-center justify-around mx-auto px-8 md:px-2 py-2 relative bg-neutral-800  md:bg-transparent mb-5 select-none">
       <div className="flex items-center">
         <Link href="/">
-          <Image
-            src={logo}
-            alt="Taskify"
-            priority
-            className="w-36 h-auto"
-          />
+          <Image src={logo} alt="Taskify" priority className="w-36 h-auto" />
         </Link>
       </div>
 
       <div className="md:flex items-center space-x-2 hidden">
+        {!session?.user && (
+          <div className="flex items-center justify-center gap-2">
+            <Link
+              href="/public"
+              className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
+            >
+              Tarefas Publicas <MdPublic size={24} />
+            </Link>
+          </div>
+        )}
         {session?.user && (
-          <Link
-            href="/dashboard"
-            className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
-          >
-            Dashboard
-          </Link>
+          <div className="flex items-center justify-center gap-2">
+            <Link
+              href="/public"
+              className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
+            >
+              Tarefas <MdPublic size={24} />
+            </Link>
+
+            <Link
+              href="/dashboard"
+              className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
+            >
+              Dashboard <TbListCheck size={24} />
+            </Link>
+          </div>
         )}
 
         {status === "loading" ? (
@@ -46,30 +61,40 @@ export function Header() {
             className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
             onClick={() => signOut()}
           >
-            Logout <FiLogOut size={24} />
+            Logout <TbLogout2 size={24} />
           </button>
         ) : (
           <button
             className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
             onClick={() => signIn()}
           >
-            Login <FiLogIn size={24} />
+            Login <TbLogin2 size={24} />
           </button>
         )}
       </div>
 
       {/* Mobile menu button or Login button */}
-      <div className="md:hidden">
-        {session ? (
-          <button className="text-white text-2xl" onClick={toggleMobileMenu}>
-            <FiMenu size={28}/>
+      <div className="md:hidden flex items-center justify-center gap-2">
+        <button
+          className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
+          onClick={toggleMobileMenu}
+        >
+          <TbMenu2 size={28} />
+        </button>
+        {session && (
+          <button
+            className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
+            onClick={() => signOut()}
+          >
+            <TbLogout2 size={28} />
           </button>
-        ) : (
+        )}
+        {!session && (
           <button
             className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
             onClick={() => signIn()}
           >
-            Login <FiLogIn size={24} />
+            <TbLogin2 size={28} />
           </button>
         )}
       </div>
@@ -78,29 +103,24 @@ export function Header() {
       {isMobileMenuOpen && (
         <div className="md:hidden absolute top-[4.9rem] left-0 w-full bg-neutral-800 py-2 rounded-b-3xl">
           <div className="flex items-center justify-center gap-2">
+            <Link
+              href="/public"
+              className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
+              onClick={toggleMobileMenu}
+            >
+              Tarefas <MdPublic size={24} />
+            </Link>
+
             {session?.user && (
-              <Link
-                href="/dashboard"
-                className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm" onClick={toggleMobileMenu}
-              >
-                Dashboard
-              </Link>
-            )}
-            {session && (
-              <button
-                className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
-                onClick={() => signOut()}
-              >
-                Logout <FiLogOut size={24} />
-              </button>
-            )}
-            {!session && (
-              <button
-                className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
-                onClick={() => signIn()}
-              >
-                Login <FiLogIn size={24} />
-              </button>
+              <div className="flex items-center justify-center gap-2">
+                <Link
+                  href="/dashboard"
+                  className="text-white flex items-center justify-center gap-2 font-medium px-3 py-2 rounded-full transition-all duration-300 ease-linear hover:bg-neutral-300/10 text-sm"
+                  onClick={toggleMobileMenu}
+                >
+                  Dashboard <TbListCheck size={24} />
+                </Link>
+              </div>
             )}
           </div>
         </div>
--- src/components/textarea/index.tsx
+++ src/components/textarea/index.tsx
@@ -4,7 +4,7 @@ export function Textarea({ ...rest }: HTMLProps<HTMLTextAreaElement>) {
   return (
     <textarea
       id="textarea"
-      className="w-full h-28 resize-none rounded-md outline-none p-2 text-sm"
+      className="w-full h-20 resize-none rounded-md outline-none p-2 text-sm"
       {...rest}
     ></textarea>
   );
